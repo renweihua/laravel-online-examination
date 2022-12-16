72af25e1e2500dd3103ac12980a48645c449f597
小丑路人 <2278757482@qq.com>
2022-12-16 19:22:05 +0800

    `学生端`课程列表 /student/courses

--- app/Modules/Student/Routes/api.php
+++ app/Modules/Student/Routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,14 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/student', function (Request $request) {
-    return $request->user();
-});
+Route::prefix('student')
+    ->group(function(){
+        Route::post('/auth/login',  'AuthController@login');
+
+        // 课程列表
+        Route::get('courses',  'StudentController@getCourses');
+
+        Route::middleware([])->group(function(){
+
+        });
+    });
